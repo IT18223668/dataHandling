b234392db4561242dbf914f9a5f30dfd369e4a3b
IT18223668 <53345447+IT18223668@users.noreply.github.com>
2020-04-08 22:26:55 +0530

    dataHandling

--- app/src/main/java/com/example/datahandling/MainActivity.java
+++ app/src/main/java/com/example/datahandling/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.datahandling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText uname,password;
+    Button button,buttonAdd,buttonSign,buttonDelete,buttonUpdate;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uname=findViewById(R.id.uname);
+        password=findViewById(R.id.password);
+
+        button=findViewById(R.id.button);
+        buttonAdd=findViewById(R.id.buttonAdd);
+        buttonSign=findViewById(R.id.buttonSign);
+        buttonUpdate=findViewById(R.id.buttonUpdate);
+        buttonDelete=findViewById(R.id.buttonDelete);
     }
 }
